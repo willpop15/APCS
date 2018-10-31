f61f32fc1e4ef27902c028136a8a161c81634fd5
wpopoli <Maggiess1>
2018-10-31 13:37:43 -0400

    initial commit

---
+++ src/WillPopoliSum.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.
+
+public class WillPopoliSum {
+    public static void main(String[] args) {
+        Scanner file = new Scanner("sum.txt");
+
+    }
+
+
+
+
+
+
+}
