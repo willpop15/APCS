871f5fcf399c93cd129c3ee742db6b7e5fdd5f95
wpopoli <Maggiess1>
2018-11-05 13:40:08 -0500

    initial commit

--- src/WillPopoliSum.java
+++ src/WillPopoliSum.java
@@ -2,8 +2,28 @@ import java.util.Arrays;
 import java.util.
 
 public class WillPopoliSum {
+    public static final int DIGIT = 25;
     public static void main(String[] args) {
         Scanner file = new Scanner("sum.txt");
+        while(file.hasNextLine()) {
+            String line = file.nextLine();
+            Scanner console = new Scanner(line);
+            String number = "" + file.nextInt();
+            while(console.hasNextInt()){
+                String current = console.next();
+                int[] num = new int[DIGIT];
+                String[] expand = current.split("");
+                for(int i = 0; i<expand.length; i++){
+                    num[i]=Integer.parseInt(expand[i]);
+                }
+
+
+            }
+
+
+        }
+
+
 
     }
 
